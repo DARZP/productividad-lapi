eb83cc5796fe02b37fb2519a5153b2bdfbac0d90
DARZP <diego.arias123@gmail.com>
2026-06-09 14:05:16 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // sw.js - Service Worker de LAPI (Versión Activa / Network First)
 
 // 🛑 CAMBIA ESTE NÚMERO CADA VEZ QUE SUBAS UNA ACTUALIZACIÓN A NETLIFY
-const CACHE_VERSION = 'lapi-pwa-v2.0.0'; 
+const CACHE_VERSION = 'lapi-pwa-v2.0.1'; 
 
 const ASSETS_TO_CACHE = [
     './',
